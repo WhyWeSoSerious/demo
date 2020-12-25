d3640e6a649dfa2bff9ba94ca191f1cbf26008dd
joker <78447095@qq.com>
2020-12-25 11:42:26 +0800

    test

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,10 @@
 // @ts-ignore
 import Home from '../views/Home'
+// @ts-ignore
 import Login from '../views/Login'
+// @ts-ignore
 import Register from '../views/Register'
+// @ts-ignore
 import Search from '../views/Search'
 export default [
     {
